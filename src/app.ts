a52120597f224deb938d7024536d78331bc2281d
yymasaki <y.masaki34@icloud.com>
2024-09-20 23:42:51 +0900

    expressを用いたルーティング

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import express from 'express';
+import express, { Request , Response, NextFunction} from 'express';
 const app = express();
+app.use('/', (req, res, next) => {
+    console.log('Hello');
+    throw new Error();
+    next();
+})
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello</h1>');
+})
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log('hello');
+    next();
+})
 app.listen(3000);
